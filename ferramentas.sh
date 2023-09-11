fa8c88b423830284f0b754c7eb9010f30ad07864
pedro-desktop <pmfs1987@gmail.com>
2023-09-11 20:36:27 +0100

    Adição da função gestao_repositorio_obtem_token, implementação da mesma na função gestao_repositorio_verifica_versao e conclusão da função gestao_repositorio_instala.

--- ferramentas.sh
+++ ferramentas.sh
@@ -65,6 +65,23 @@ function menu_texto_simples () {
     done
 }
 
+function gestao_repositorio_obtem_token () {
+    
+    # Função que pede input ao utilizador e verifica se o input fornecido é um personal token válido de um utilizador do GitHub.
+    # A função preenche a variável gestao_repositorio_token, usada noutras funções.
+    # A função devolve estado 0 quando deteta um personal token válido (e é guardado na variável gestao_repositorio_token).
+
+    unset gestao_repositorio_token
+    echo -e "Para continuar é necessário introduzir um personal token do GitHub."
+    while true
+    do
+        read -p "Personal token: " gestao_repositorio_token
+        gestao_repositorio_obtem_token_codigo=$(curl -s -o /dev/null -w "%{http_code}" -H "Authorization: token $gestao_repositorio_token" https://api.github.com/user)
+        [ $gestao_repositorio_obtem_token_codigo = 200 ] && return 0
+        echo "Token inválido."
+    done
+}
+
 function gestao_repositorio_verifica_publicidade () {
 
     # Função que verifica se um repositório é público na respetiva lista de um utilizador no GitHub.
@@ -90,6 +107,7 @@ function gestao_repositorio_verifica_versao () {
 
     # Função que verifica se um repositório presente no computador está na última versão disponível no GitHub.
     # Utiliza a função gestao_repositorio_verifica_publicidade.
+    # Utiliza a função gestao_repositorio_obtem_token (no caso do repositório ser privado).
     # Pressupõe que o repositório já está presente localmente.
     # Pressupõe que o conjunto dos scripts está localizado em "$HOME/Scripts/"
     # Pressupõe que, sem uso do git, as versões instaladas dos repositórios estejam escritas no ficheiro "$HOME/Scripts/.versões/nome_do_repositorio_no_GitHub".
@@ -97,50 +115,53 @@ function gestao_repositorio_verifica_versao () {
     #   1º argumento - nome do utilizador no GitHub;
     #   2º argumento - nome do repositório no GitHub;
     #   3º argumento - nome da pasta onde o repositório está localmente.
-    # Caso o repositório seja privado, é necessário introduzir manualmente, a pedido do script, um personal token válido do GitHub.
     # Devolve os seguintes estados:
     #   estado 0 quando a versão do repositório no GitHub é igual ou inferior à versão do repositório local.
     #   estado 1 nos seguintes erros:
     #       - o repositório não tem versão de release;
     #       - erro na função gestao_repositorio_verifica_publicidade (estado dessa função != 0 e != 2);
+    #       - erros desconhecidos (quando não consegue registar na variável gestao_repositorio_verifica_versao_local a versão local do repositório);
     #   estado 2 quando a versão do repositório no GitHub é superior à versão do repositório local.
-    #   estado 3 quando o repositório não está presente em "$HOME/Scripts/nome do repositório" ou quando não existe ficheiro com a versão (em "$HOME/Scripts/.versões/nome_do_script").
+    #   estado 3 quando o repositório não está presente em "$HOME/Scripts/nome do repositório".
+    #   estado 4 quando não existe nem a pasta .git nem o ficheiro com a versão ("$HOME/Scripts/.versões/nome_do_repositório_no_repositório_no_GitHub")
     # Pode ser utilizado gravando, imediatamente a seguir ao uso da função, o valor contido em $? numa variável.
 
-    unset $gestao_repositorio_verifica_versao_local
-    unset $gestao_repositorio_verifica_versao_github
+    unset gestao_repositorio_verifica_versao_local
+    unset gestao_repositorio_verifica_versao_github
     gestao_repositorio_verifica_publicidade "$1" "$2"
     gestao_repositorio_verifica_versao_resultado=$?
-    if [ $gestao_repositorio_verifica_versao_resultado = 0 ]
+    if [ $gestao_repositorio_verifica_versao_resultado = 0 ] # quando o repositório é público
     then
         gestao_repositorio_verifica_versao_github=$(curl -s https://api.github.com/repos/"$1"/"$2"/releases/latest | grep -Po " {2}\"tag_name\": \"\K[^\"]+(?=\",)")
-    elif [ $gestao_repositorio_verifica_versao_resultado = 2 ]
+    elif [ $gestao_repositorio_verifica_versao_resultado = 2 ] # quando o repositório não é público
     then
-        echo -e "O repositório escolhido poderá ser privado. Introduza um personal token do GitHub válido."
-        read gestao_repositorio_verifica_versao_token
-        gestao_repositorio_verifica_versao_github=$(curl -sH "Authorization: token $gestao_repositorio_verifica_versao_token" https://api.github.com/repos/"$1"/"$2"/releases/latest | grep -Po " {2}\"tag_name\": \"\K[^\"]+(?=\",)")
+        [ -z "$gestao_repositorio_token" ] && gestao_repositorio_obtem_token
+        gestao_repositorio_verifica_versao_github=$(curl -sH "Authorization: token $gestao_repositorio_token" https://api.github.com/repos/"$1"/"$2"/releases/latest | grep -Po " {2}\"tag_name\": \"\K[^\"]+(?=\",)")
     fi
     [ -z "$gestao_repositorio_verifica_versao_github" ] && return 1
     if [ -d "$HOME/Scripts/$3" ]
     then
         if [ -d "$HOME/Scripts/$3/.git" ]
         then
+            gestao_repositorio_verifica_versao_caminho=$(pwd)
             cd "$HOME/Scripts/$3"
-
+            gestao_repositorio_verifica_versao_local=$(git describe --tags)
+            cd "$gestao_repositorio_verifica_versao_caminho"
         else
-            [ -f "$HOME/Scripts/.versões/$2" ] && gestao_repositorio_verifica_versao_local=$(cat "$HOME/Scripts/.versões/$2")
+            [ -f "$HOME/Scripts/.versões/$2" ] && gestao_repositorio_verifica_versao_local=$(cat "$HOME/Scripts/.versões/$2") || return 4
         fi
     else
         return 3
     fi
-    [ -z "$gestao_repositorio_verifica_versao_local" ] && return 3
+    [ -z "$gestao_repositorio_verifica_versao_local" ] && return 1
     gestao_repositorio_verifica_versao_temporario=$IFS
+    IFS='.'
     read -r -a gestao_repositorio_verifica_versao_github_numeros <<< $gestao_repositorio_verifica_versao_github
     read -r -a gestao_repositorio_verifica_versao_local_numeros <<< $gestao_repositorio_verifica_versao_local
     IFS=$gestao_repositorio_verifica_versao_temporario
     for gestao_repositorio_verifica_versao_iteracao in "${!gestao_repositorio_verifica_versao_github_numeros[@]}"
     do
-        [[ ${gestao_repositorio_verifica_versao_github_numeros[$gestao_repositorio_verifica_versao_iteracao]} > ${gestao_repositorio_verifica_versao_local_numeros[gestao_repositorio_verifica_versao_iteracao]}]] && return 2
+        [[ ${gestao_repositorio_verifica_versao_github_numeros[$gestao_repositorio_verifica_versao_iteracao]} > ${gestao_repositorio_verifica_versao_local_numeros[gestao_repositorio_verifica_versao_iteracao]} ]] && return 2
     done
     return 0
 }
@@ -148,5 +169,45 @@ function gestao_repositorio_verifica_versao () {
 function gestao_repositorio_instala () {
 
     # Função que transfere do GitHub e move um repositório para $HOME/Scripts/pasta e regista a versão correspondente em $HOME/Scripts/.versões/nome_do_repositório_no_GitHub.
-    echo "Função que saca e move um repositório do GitHub para $HOME/Scripts/"
+    # Utiliza a função gestao_repositorio_verifica_publicidade.
+    # Utiliza a função gestao_repositorio_obtem_token.
+    # Pressupõe que o conjunto dos scripts está localizado em "$HOME/Scripts/"
+    # Pressupõe que, sem uso do git, as versões instaladas dos repositórios estejam escritas no ficheiro "$HOME/Scripts/.versões/nome_do_repositorio_no_GitHub".
+    # Necessita dos seguintes argumentos:
+    #   1º argumento - nome do utilizador no GitHub;
+    #   2º argumento - nome do repositório no GitHub;
+    #   3º argumento - nome da pasta onde o repositório será instalado localmente.
+    # Devolve os seguintes estados:
+    #   estado 0 quando a instalação é bem sucedida.
+    #   estado 1 quando há falhas na instalação.
+    #   estado 3 quando no caminho de instalação corresponde um acompanhamento do git (ou seja, quando existe a pasta .git no referido caminho).
+     
+    [ -d "$HOME/Scripts/$3/.git" ] && return 3
+    gestao_repositorio_verifica_publicidade "$1" "$2"
+    gestao_repositorio_instala_resultado=$?
+    if [ $gestao_repositorio_instala_resultado = 0 ] # quando o repositório é público
+    then
+        gestao_repositorio_instala_url=$(curl -s "https://api.github.com/repos/$1/$2/releases/latest" | grep -Po " {2}\"tarball_url\": \"\K[^\"]+(?=\",)")
+        curl -LJO "$gestao_repositorio_instala_url"
+    elif [ $gestao_repositorio_instala_resultado = 2 ] # quando o repositório não é público
+    then
+        [ -z "$gestao_repositorio_token" ] && gestao_repositorio_obtem_token
+        gestao_repositorio_instala_url=$(curl -sH "Authorization: token $gestao_repositorio_token" "https://api.github.com/repos/$1/$2/releases/latest" | grep -Po " {2}\"tarball_url\": \"\K[^\"]+(?=\",)")
+        curl -LJOH "Authorization: token $gestao_repositorio_token" "$gestao_repositorio_instala_url"
+    else
+        return 1
+    fi
+    gestao_repositorio_instala_versao=$(echo "$gestao_repositorio_instala_url" | grep -Po "tarball/\K[\d\.]+(?=$)")
+    tar -xf "${1}-${2}-${gestao_repositorio_instala_versao}*.tar.gz"
+    rm "${1}-${2}-${gestao_repositorio_instala_versao}*.tar.gz"
+    mv "${1}-${2}-*/" "$3"
+    if [ -d "$HOME/Scripts/" ]
+    then
+        [ -d "$HOME/Scripts/$3" ] && rm -r "$HOME/Scripts/$3"
+    else
+        mkdir "$HOME/Scripts"
+    fi
+    mv "$3" "$HOME/Scripts/"
+    [ -d "$HOME/Scripts/.versões" ] || mkdir "$HOME/Scripts/.versões"
+    echo "$gestao_repositorio_instala_versao" > "$HOME/Scripts/.versões/$2"
 }
